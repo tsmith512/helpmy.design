d67d04e273184ea8b0d6ca62a80ec08dc16ddb1e
Taylor Smith <tsmith@tsmithcreative.com>
2015-05-14 15:51:36 +0000

    Load the articles json and output on the console for debugging

--- js/main.js
+++ js/main.js
@@ -1,5 +1,23 @@
 (function(){
   'use strict';
 
-  alert('Hello');
+  // Load the Articles
+  var request = new XMLHttpRequest();
+  request.open('GET', '/js/articles.json', true);
+
+  request.onload = function() {
+    if (this.status >= 200 && this.status < 400) {
+      // Success!
+      var data = JSON.parse(this.response);
+      console.log(data);
+    } else {
+      console.log('Response Error ' + this.status);
+    }
+  };
+
+  request.onerror = function() {
+    console.log('Request Error');
+  };
+
+  request.send();
 })();
