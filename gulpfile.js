6762914c1d88db8ab47b35ef927a2dffb300f00d
Taylor Smith <tsmith@tsmithcreative.com>
2015-05-14 15:44:18 +0000

    For early WIP: just copy index.html and js into the build directory on save in dev-watch task

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,30 @@
 var gulp = require('gulp'),
+    watch = require('gulp-watch'),
     yaml = require('gulp-yaml');
 
+// Convert the YAML indexes to JSON
 gulp.task('index', function() {
-  gulp.src('./links/*.yml')
+  gulp.src('links/*.yml')
     .pipe(yaml({ safe: true }))
-    .pipe(gulp.dest('./_dist/js/'))
+    .pipe(gulp.dest('_dist/js/'));
+});
+
+// For Dev, just copy the HTML, JS, and CSS straight to the build directory
+gulp.task('dev', function() {
+  gulp.src('index.html')
+    .pipe(gulp.dest('_dist/'));
+  gulp.src('js/**/*.js')
+    .pipe(gulp.dest('_dist/js/'));
+});
+
+gulp.task('dev-watch', function () {
+  watch("js/**/*.js", function() {
+    gulp.start("dev");
+  });
+  watch("*.html", function() {
+    gulp.start("dev");
+  });
+  watch("links/*.yml", function() {
+    gulp.start("index");
+  });
 });
