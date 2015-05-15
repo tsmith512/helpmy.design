5047b3655edc2b3a4d28acd98f7ef2cfe1122ce6
Taylor Smith <tsmith@tsmithcreative.com>
2015-05-15 00:12:04 +0000

    Add tag classes to each article. May do this differently later...

--- js/main.js
+++ js/main.js
@@ -2,6 +2,9 @@
   'use strict';
 
   var presentArticles = function(data) {
+    data.articles.forEach(function(item, i){
+      item.id = i;
+    });
     console.log(data);
     dust.render('article.html', data, function(err, out) {
       document.getElementById('index').innerHTML = out;
